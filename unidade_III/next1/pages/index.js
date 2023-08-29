4439e28699ad443033adf65c39ad7e5df34f980c
melquetrindade <melquetrindade654@gmail.com>
2023-08-28 21:52:53 -0300

    instalação do next js e finalizando a receita sobre next #1

---
+++ unidade_III/next1/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Tabela} from './tabela.js';
+
+export default function Home(){
+    return (
+        <div>
+            <h2>
+                Viva Santana!
+            </h2>
+            <Tabela />
+        </div>
+    )
+}
---
+++ unidade_III/next2/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home(){
+    return (
+        <div>
+            <h2>
+                Viva Santana! 3
+            </h2>
+        </div>
+    )
+}

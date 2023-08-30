de5278486da640b0f2f1e0e01904e0db3721d25d
melquetrindade <melquetrindade654@gmail.com>
2023-08-30 14:17:07 -0300

    atualizando a receita java script 1

--- unidade_I/javaScript_basico/script.js
+++ unidade_I/javaScript_basico/script.js
@@ -1,7 +1,6 @@
 function carregarDiv(){
     let divH1 = document.getElementById("nome_cerveja")
     divH1.classList.toggle('hide')
-    divH1.style.transition = '20s'
 }
 
 let botao = document.getElementById("botaoCarregar")

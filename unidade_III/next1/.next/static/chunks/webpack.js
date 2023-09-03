6d3a34220f9c399b18e9df5e4851265e31f5302b
melquetrindade <melquetrindade654@gmail.com>
2023-09-03 16:14:39 -0300

    continuando o desenvolvimento da receita sobre next #1

--- unidade_III/next1/.next/static/chunks/webpack.js
+++ unidade_III/next1/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a05e7f70d0c699b6"; }
+/******/ 		__webpack_require__.h = function() { return "858e80746cc6b144"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
@@ -1216,6 +1216,11 @@
 /******/ 		var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/nonce */
+/******/ 	!function() {
+/******/ 		__webpack_require__.nc = undefined;
 /******/ 	}();
 /******/ 	
 /************************************************************************/
--- unidade_III/next1/pages/index.js
+++ unidade_III/next1/pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Tabela} from './tabela.js';
+import styles from '../estilos/style.module.css'
+
+// Comandos úteis:
+// Carregar a aplicação: npm run dev
+// Finalizar a aplicação: Ctrl + c
 
 export default function Home(){
     return (
-        <div>
+        <div className={styles.container}>
             <h2>
                 Viva Santana!
             </h2>
-            <Tabela />
+            <Tabela/>
         </div>
     )
 }

8b1fafca87c4a2590b9549062ecd63f7332a5caf
melquetrindade <melquetrindade654@gmail.com>
2023-10-15 11:20:12 -0300

    concluindo a receita 9 de next

--- unidade_III/next9/components/navBar.js
+++ unidade_III/next9/components/navBar.js
@@ -9,25 +9,10 @@ export default function NavBar(){
                 <Link href='/'>HOME</Link>
             </li>
             <li>
-                <Link href='/movies'>FILMES 1</Link>
+                <Link href='/movies/tt0033152'>RECEITA 9.1</Link>
             </li>
             <li>
-                <Link href='/movies2'>FILMES 2</Link>
-            </li>
-            <li>
-                <Link href='/movies3'>FILMES 3</Link>
-            </li>
-            <li>
-                <Link href='/movies4'>FILMES 4</Link>
-            </li>
-            <li>
-                <Link href='/movies/tt0033152'>FILMES 5.1</Link>
-            </li>
-            <li>
-                <Link href='/github/melquetrindade'>FILMES 5.2</Link>
-            </li>
-            <li>
-                <Link href='/infoCliente'>INFO CLIENTE</Link>
+                <Link href='/github/melquetrindade'>RECEITA 9.2</Link>
             </li>
         </ul>
     )

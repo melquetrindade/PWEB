f2a9e89641893dd1f8e0690f61639314322aafd8
melquetrindade <melquetrindade654@gmail.com>
2023-11-13 15:58:15 -0300

    finalizando a receita final

--- unidade_III/receita_final/pages/index.js
+++ unidade_III/receita_final/pages/index.js
@@ -3,10 +3,35 @@ import styles from '../styles/index.module.css'
 
 export default function Home(){
     return (
-        <>
-            <h1>
-                Página Principal
-            </h1>
-        </>
+        <main>
+            <div className={styles.content}>
+                <h1>
+                    Receitas de Next
+                </h1>
+                <ul>
+                    <li>
+                        <a href='https://pweb-pimq.vercel.app/' target='_blank'>Receita 1</a>
+                    </li>
+                    <li>
+                        <a href='https://pweb-reeb.vercel.app/' target='_blank'>Receita 2</a>
+                    </li>
+                    <li>
+                        <a href='https://front-end-vikb.vercel.app/' target='_blank'>Receita 3</a>
+                    </li>
+                    <li>
+                        <a href='https://pweb-six.vercel.app/' target='_blank'>Receita 4</a>
+                    </li>
+                    <li>
+                        <a href='https://receitas5-6-9-3bm2.vercel.app/movies3' target='_blank'>Receita 5</a>
+                    </li>
+                    <li>
+                        <a href='https://receita6-next.vercel.app/' target='_blank'>Receita 6</a>
+                    </li>
+                    <li>
+                        <a href='https://receita9-six.vercel.app/' target='_blank'>Receita 9</a>
+                    </li>
+                </ul>
+            </div>
+        </main>
     )
 }
